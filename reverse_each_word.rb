208e5a8595c8a1b580613ae588494fc545eaa132
Nkosi Phillip <internap627@gmail.com>
2018-09-15 02:47:22 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,6 @@
-def reverse_each_word(str)
-newArray = []
-str.split().each {|word| newArray.push(word.reverse) }
-
-return newArray.join(" ")
-
-end
-
 def reverse_collect_word(str)
 
 str.split().collect {|word| word.reverse }.join(" ")
+
 
 end
